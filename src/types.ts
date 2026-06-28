cfb12032b483319d8e3b425a920db799e056a79f
Felix-au <felixaugum@gmail.com>
2026-06-29 02:47:51 +0530

    Set default colors to blue, red, green, yellow

--- src/types.ts
+++ src/types.ts
@@ -22,7 +22,7 @@ export const DEFAULT_SETTINGS: AppSettings = {
   particleCount: 7000,
   particleSize: 3.0,
   particleOpacity: 0.75,
-  colors: ["#0008ff", "#15846e", "#ff0000", "#ffb829"], // Blue, Green, Red, Yellow
+  colors: ["#0000ff", "#ff0000", "#00ff00", "#ffb829"], // Blue, Red, Green, Yellow
   interactionMode: "ripple",
   interactionForce: 2.0,
   interactionRadius: 100,
